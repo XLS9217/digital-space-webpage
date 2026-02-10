da3dedadb5d4e436714084ecbec80b82a9ff9b1c
xls_shgbit <loki_xls@outlook.com>
2026-02-10 14:31:56 +0800

    Add `DebugBlock`, `CoordDisplayer`, and `CommonComponent.css` to `DebugPanel` for expandable debug UI elements and attribute display.

---
+++ packages/digital-space-tool/DebugPanel/CommonComponent/CoordDisplayer.jsx
@@ -0,0 +1,2 @@
+// This is mainly for displaying xyz like Pos Rot Scale for Model of what we have currently
+// One of this should be exactly one line of attribute and this should put in DebugBlock
---
+++ packages/digital-space-tool/DebugPanel/CommonComponent/DebugBlock.jsx
@@ -0,0 +1,2 @@
+// this is the expendable thing for light and model currently
+// after refactor this, The Control section should have a title and one of this block, and Model&Light List should be like a title and a bunch of <DebugBlock>
