16e625a1fa93315f868af77d1c581e8d7b8053b0
xls_shgbit <loki_xls@outlook.com>
2026-03-10 14:15:43 +0800

    Add `WebRegistry` class with upload and download function placeholders; export as default for future integration.

---
+++ packages/digital-space-toolkit/WebRegistry.js
@@ -0,0 +1,20 @@
+/**
+ * To claude code: change the comment after you impletement
+ * Look at the Tag Registry, it is similar
+ * For here, I need a fixed set, upload and download function
+ * they are variable, and will be null at default
+ * then the upload and download button will use those
+ * if function is null, then the upload/download button will be unclickable like the uneditable state in coorddisplayer
+ * showing tip "need to register xxxx"
+ * if function is registered, then the upload and download button will call it
+ * don't forget to export this in the correct index.js
+ */
+class WebRegistry{
+
+    register_upload
+
+    register_download
+}
+
+const webRegistry = new WebRegistry();
+export default webRegistry;
