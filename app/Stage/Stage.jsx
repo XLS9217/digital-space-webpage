17a17a9a6acdfca6c1f3f3055f3d5df96311dc36
xls_shgbit <loki_xls@outlook.com>
2026-03-12 20:20:10 +0800

    add button in stage

--- app/Stage/Stage.jsx
+++ app/Stage/Stage.jsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react'
+import { useEffect, useRef } from 'react'
 import { DigitalSpace, DigitalScene, tagRegistry, dataRegistry } from 'digital-space-toolkit'
 import { getSceneByName, downloadSceneZip, upsertScene } from "../API/gateway.js";
 import {ClassroomTag, MeetingTag} from './TagWithStyle.jsx'
@@ -7,6 +7,8 @@ import './Stage.css'
 
 export default function Stage()
 {
+    const sceneController = useRef();
+
     useEffect(() => {
         tagRegistry
             .register('CLASSROOM', ClassroomTag, { distanceFactor: 40 })
@@ -25,10 +27,17 @@ export default function Stage()
     }, [])
 
     return (
-        <DigitalSpace
-            debug={true}
-        >
-            <DigitalScene sceneName="beijing_white" />
-        </DigitalSpace>
+        <>
+            <div className="layer-buttons">
+                <button onClick={() => sceneController.current?.investigateLayer('LevelDisplay', 0)}>Layer 4</button>
+                <button onClick={() => sceneController.current?.investigateLayer('LevelDisplay', 1)}>Layer 3</button>
+                <button onClick={() => sceneController.current?.investigateLayer('LevelDisplay', 2)}>Layer 2</button>
+                <button onClick={() => sceneController.current?.investigateLayer('LevelDisplay', 3)}>Layer 1</button>
+            </div>
+
+            <DigitalSpace debug={true}>
+                <DigitalScene sceneName="beijing_white" controllerRef={sceneController} />
+            </DigitalSpace>
+        </>
     )
 }
